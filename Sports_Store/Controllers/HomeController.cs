a6108b11acdd19036375999260c4a5a93f819a1e
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2026-01-19 19:04:44 +0400

    Views && Pagination

--- Sports_Store/Controllers/HomeController.cs
+++ Sports_Store/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Sports_Store.Models;
 
 namespace Sports_Store.Controllers;
 
 public class HomeController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private IStoreRepository repository;
+
+    public int PageSize = 4;
+
+    public HomeController(IStoreRepository repo)
     {
-        return View();
+        repository = repo;
     }
+
+        public ViewResult Index(int productPage = 1)
+            => View(repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
+            
+    
+    
 }
